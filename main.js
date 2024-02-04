7783a60d9a5c8fa07eefd09872f5ec4585d6ea8f
Shayan <148603453+Shyaan921@users.noreply.github.com>
2024-02-04 17:50:00 +0500

    Completed the calculator for the most part
    need to make changes so that first number can be negative.

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 function add(first, second) {
-    return first + second;
+    return +first + +second;
 }
 
 function subtract(first, second) {
-    return first - second;
+    return +first - +second;
 }
 
 function multiply(first, second) {
-    return first * second;
+    return +first * +second;
 }
 
 function divide(first, second) {
     if (second === 0) {
         return "ERROR"
     } else {
-        return (first / second).toFixed(2);
+        return (+first / +second).toFixed(2);
     };
 }
 
 function operate(first, second, operator) {
+    if ((first === '') || (second === '') || (!['+', '-', '×', '÷'].includes(operator))) {
+        return "ERROR"
+    }
+
     if (operator === '+') {
         return add(first, second);
     } else if (operator === '-') {
@@ -27,31 +31,73 @@ function operate(first, second, operator) {
         return multiply(first, second);
     } else if (operator === '÷') {
         return divide(first, second);
-    } else {
-        return 'ERROR'
-    }
+    } 
 };
+
+function reset() {
+    firstOperator = true
+    firstNum = 0
+    secondNum = 0
+    operator = ''
+    equation = ''
+    equ.textContent = ''
+}
+
+let firstOperator = true;
+let firstNum;
+let secondNum;
+let operator;
+let tempOperator;
+let equation = '';
+let equ = document.querySelector('#equation');
+let ans = document.querySelector("#answer");
 
 let invisibleBtns = document.querySelectorAll('.invisble')
 for (let i = 0; i < invisibleBtns.length; i++) {
     invisibleBtns[i].disabled = true;
 };
 
-let operation = [];
 let btns = document.querySelectorAll('button')
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', () => {
-        operation.push(btns[i].textContent);
-        let equation = document.querySelector('#equation')
-        
-        if (operation.length === 0) {
-            equation.textContent = operation[i]
-        } else {
-            equation.textContent = equation.textContent + ' ' + operation[operation.length - 1]
+        if (btns[i].textContent === 'AC') {
+            reset()
+            ans.textContent = ''
+            previousAnswer = ''
+        } else if (!['+', '-', '×', '÷', 'AC', '='].includes(btns[i].textContent)) {
+            equ.textContent += btns[i].textContent;
+            equation += btns[i].textContent;
+        } else if (['+', '-', '×', '÷'].includes(btns[i].textContent)) {
+            equ.textContent += ' ' + btns[i].textContent + ' ';
+            if (firstOperator === true) {
+                firstOperator = false
+                firstNum = equation;
+                operator = btns[i].textContent;
+                equation = equation.slice(0, 0)
+            } else {
+                secondNum = equation
+                tempOperator = btns[i].textContent
+                firstNum = operate(firstNum, secondNum, operator)
+                previousAnswer = firstNum;
+                secondNum = 0;
+                operator = tempOperator;
+                ans.textContent = firstNum
+                equation = equation.slice(0, 0)
+            }
+        } else if (btns[i].textContent === '=') {
+            if ((!secondNum) && (!operator)) {
+                firstNum = equation;
+                ans.textContent = equation
+                reset() 
+            } else {
+                secondNum = equation
+                ans.textContent = operate(firstNum, secondNum, operator)
+                reset()
+            }
         }
+
     });
 };
-
 
 
 
